2934fc027eebc7c7af9329d9af689aaff7f91c40
Frederico Cerveira <frederico.cerveira@gmail.com>
2024-03-06 11:49:10 +0000

    Fixes

--- backend/framework/conf/current.py
+++ backend/framework/conf/current.py
@@ -2,11 +2,6 @@
 Global configuration file that defines the properties of each campaign
 """
 
-import framework.campaigns.AI4EU
-import framework.campaigns.sw_faults_example
-import framework.campaigns.ExampleCampaign
-import framework.campaigns.FailureCampaign
-
 predefined_cols = ()
 
 main_csv_path = "table.csv" # Path to the CSV that will keep the main results
